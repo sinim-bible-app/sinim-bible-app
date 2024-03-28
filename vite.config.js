cc37acbc4b87788b6ded1a354cc9d651028077ca
Caleb White <cdwhite3@pm.me>
2024-03-28 11:17:50 -0500

    chore: move entrypoint to root dir

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from "vite"
 import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
-    root: "src",
-    build: {
-        outDir: "../dist",
-        emptyOutDir: true,
-    },
     plugins: [
         viteSingleFile(),
     ],
