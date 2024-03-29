5d6880bc31bb7948c80f82e297c460ac5f66a146
Caleb White <cdwhite3@pm.me>
2024-03-29 11:27:17 -0500

    feat: install and configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "selector",
+  content: [
+    './index.html',
+    './src/**/*.{js,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
