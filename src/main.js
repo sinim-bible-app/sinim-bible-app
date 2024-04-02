d23836b38b0ba060e755c449c72f997873f7cb2b
Caleb White <cdwhite3@pm.me>
2024-04-01 21:43:29 -0500

    feat: add favicon

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ import router from "@/router";
 
 import bibleData from "@/assets/translations/zh/chinese_union_version.json";
 
+import favicon from "@/assets/favicon.png";
+
+const faviconLink = document.createElement("link");
+faviconLink.rel = "icon";
+faviconLink.href = favicon;
+document.head.appendChild(faviconLink);
+
 createApp(App)
     .use(createPinia())
     .use(router)
