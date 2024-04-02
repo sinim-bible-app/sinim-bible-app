b2cec3af52070c77ebc54c81e9faa9ac21c223ad
Caleb White <cdwhite3@pm.me>
2024-04-02 00:29:58 -0500

    feat: add dark mode component

---
+++ src/components/DarkMode.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import { useDark, useToggle } from "@vueuse/core";
+
+    const isDark = useDark();
+    const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+    <button @click="toggleDark()">
+        {{ isDark ? "☀️" : "🌙" }}
+    </button>
+</template>
