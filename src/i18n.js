0e4d84e4a295022a8b80480787042b52b912b88a
Caleb White <cdwhite3@pm.me>
2024-04-08 07:47:22 -0500

    feat: detect browser locale

---
+++ src/i18n.js
@@ -0,0 +1,30 @@
+import { createI18n } from "vue-i18n";
+import messages from "@intlify/unplugin-vue-i18n/messages";
+
+// https://stackoverflow.com/a/52112155
+const getNavigatorLanguage = () => {
+    if (navigator.languages && navigator.languages.length) {
+        return navigator.languages[0];
+    } else {
+        return (
+            navigator.userLanguage ||
+            navigator.language ||
+            navigator.browserLanguage ||
+            "en"
+        );
+    }
+};
+
+const locale = getNavigatorLanguage();
+
+document.documentElement.lang = locale;
+
+const i18n = createI18n({
+    legacy: false,
+    allowComposition: true,
+    locale,
+    fallbackLocale: "en",
+    messages,
+});
+
+export default i18n;
--- src/main.js
+++ src/main.js
@@ -2,11 +2,10 @@ import "@/app.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { createI18n } from "vue-i18n";
-import messages from "@intlify/unplugin-vue-i18n/messages";
 
 import App from "@/App.vue";
 import router from "@/router";
+import i18n from "@/i18n";
 
 import favicon from "@/assets/favicon.png";
 
@@ -14,14 +13,5 @@ const faviconLink = document.createElement("link");
 faviconLink.rel = "icon";
 faviconLink.href = favicon;
 document.head.appendChild(faviconLink);
-
-const i18n = createI18n({
-    legacy: false,
-    allowComposition: true,
-    // locale: "en",
-    locale: "zh",
-    fallbackLocale: "en",
-    messages,
-});
 
 createApp(App).use(createPinia()).use(router).use(i18n).mount("#app");
