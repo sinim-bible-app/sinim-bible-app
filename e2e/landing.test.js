9969445e5eba6052da487e54ce0065b82e5e3fb4
Caleb White <cdwhite3@pm.me>
2024-08-10 18:54:11 -0500

    test: rename test

--- e2e/landing.test.js
+++ e2e/landing.test.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
 
-test("visits the app root url", async ({ page, baseURL }) => {
+test("it visits the app root url", async ({ page, baseURL }) => {
     await page.goto(baseURL);
     await expect(page).toHaveTitle("Sinim Bible App");
 });
